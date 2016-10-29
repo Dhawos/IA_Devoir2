975a686b9d960ca334ce80db33b1515de42f4741
Asukero <thomas.fossati59@gmail.com>
2016-10-29 14:55:35 -0400

    corrected bad input entry on controller

--- src/ca/uqac/ia_devoir2/controllers/TextTileController.java
+++ src/ca/uqac/ia_devoir2/controllers/TextTileController.java
@@ -2,6 +2,7 @@ package ca.uqac.ia_devoir2.controllers;
 
 import ca.uqac.ia_devoir2.model.Position;
 import ca.uqac.ia_devoir2.model.SudokuGrid;
+import ca.uqac.ia_devoir2.model.Tile;
 import ca.uqac.ia_devoir2.view.TextTile;
 
 import javax.swing.event.DocumentEvent;
@@ -17,24 +18,30 @@ public class TextTileController implements DocumentListener {
 
     @Override
     public void insertUpdate(DocumentEvent e) {
-
-
+        updateValue(e);
     }
 
     @Override
     public void removeUpdate(DocumentEvent e) {
-
+        updateValue(e);
     }
 
     @Override
     public void changedUpdate(DocumentEvent e) {
+
+    }
+
+    private void updateValue(DocumentEvent e) {
         if (e.getDocument().getProperty("parent") instanceof TextTile) {
             TextTile textTile = (TextTile) e.getDocument().getProperty("parent");
             Position tilePosition = textTile.getPosition();
             try {
-                sudokuGrid.getTile(tilePosition).setValue(Integer.parseInt(textTile.getText()));
-            }
-            catch (NumberFormatException ex){
+                if (!textTile.getText().toString().equals(sudokuGrid.getTile(tilePosition).getValue().toString()))
+                    if (!textTile.getText().equals(""))
+                        sudokuGrid.getTile(tilePosition).setValue(Integer.parseInt(textTile.getText()));
+                    else
+                        sudokuGrid.getTile(tilePosition).setValue(Tile.NOT_SET_VALUE);
+            } catch (Exception ex) {
                 System.out.println(ex.getMessage());
             }
 
